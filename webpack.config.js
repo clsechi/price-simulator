ee603f99ab34fc7f4d5be9228844746d257359b2
Carlos Sechi <carlos.sechi@frentecorretora.com.br>
2019-03-01 18:13:05 -0300

    Add webpack config
    WIP: old browser support

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: ["@babel/polyfill", "./index.js"],
+  output: {
+    filename: 'priceSimulator.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist/*']),
+  ],
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['@babel/preset-env']
+  //         }
+  //       }
+  //     }
+  //   ]
+  // },
+  stats: {
+    colors: true,
+  },
+};
